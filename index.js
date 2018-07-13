ef41b61101f6999fcf02d160aa86bc394a9841cf
lokesh yadav <lokesh_rbs@yahoo.com>
2018-07-13 14:24:22 +0530

    Assignment 7.2

---
+++ index.js
@@ -0,0 +1,15 @@
+/// Javascript code to print multiple of 5 till 50
+
+// for loop
+console.log('Using for loop');
+for(var idx = 1 ; idx <= 10 ; ++idx){
+    console.log('5 x '+idx+' = ' + 5*idx);
+}
+
+// while loop
+console.log('using while loop');
+idx = 1;
+while(idx <= 10){
+    console.log('5 x '+idx+' = ' + 5*idx);
+    ++idx;
+}
